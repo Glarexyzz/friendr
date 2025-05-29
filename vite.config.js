55c500112bea98ae9ef6c3ea4576a9e20b303e82
lh1022 <luc.hutton22@imperial.ac.uk>
2025-05-29 13:18:46 +0000

    lh - fix: add host to allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['cloud-vm-43-101.doc.ic.ac.uk']
+  }
 })
