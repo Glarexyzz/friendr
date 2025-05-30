48d7fbed388babd50a1a5bfa19faa1018aab4a72
Luc Hutton <lh1022@ic.ac.uk>
2025-05-30 13:03:50 +0100

    Correct vite config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['cloud-vm-43-101.doc.ic.ac.uk']
+    allowedHosts: true
   }
 })
