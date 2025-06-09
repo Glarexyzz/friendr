d1f04e882252e9d07d946739e9a275a189aa52f5
Luc Hutton <lh1022@ic.ac.uk>
2025-06-09 20:39:28 +0100

    lh - feat: create selector component

---
+++ src/components/Selector.jsx
@@ -0,0 +1,19 @@
+const data = {
+  "city": ["London", "Oxford", "Truro", "Cambridge", "Liverpool"],
+  "university": ["Imperial", "UCL", "Oxford", "Cambridge", "Warwick"],
+};
+
+function Selector({ type }) {
+  const values = data[type];
+  const options = values.map(
+    v => <option value={v} key={crypto.randomUUID()}>{v}</option>
+  );
+
+  return (
+    <select name={type} className="selector">
+      {options}
+    </select>
+  );
+}
+
+export default Selector;
--- src/pages/Finder.jsx
+++ src/pages/Finder.jsx
@@ -1,13 +1,12 @@
+import Selector from "../components/Selector";
+
 function Finder() {
   return (
     <div>
       <h2>Potential Connections</h2>
       <div id="filters">
-        <p>Hobbies v</p>
-        <p>Location v</p>
-        <p>University v</p>
-        <p>Gender v</p>
-        <p>etc. v</p>
+        <Selector type="city" />
+        <Selector type="university" />
       </div>
       <div id="results">
         <div className="result">
